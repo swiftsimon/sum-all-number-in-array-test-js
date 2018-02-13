c8a5a6f5f576c94dbafba6d23e6bac83dd4ebcb3
Simon <simon@simonhamilton.ca>
2018-02-13 18:56:37 +0000

    recursion to sum array of arrays

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function sumItems(array) {
+  let sum = 0;
+
   // Sum all the numbers in the array
+  for (item of array) {
+    if(Array.isArray(item)) {
+
+        sum += sumItems(item)
+
+      } else {
+        sum += item;
+      }
+
+    }
+    return sum;
 }
 
 module.exports = sumItems;
